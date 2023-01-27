dd3a9506651e7fc762d39d6e06dc525f44e0078a
themoonset <hgarcia0805@gmail.com>
2023-01-27 13:00:39 -0600

    blah

---
+++ runNode.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = 'hugolearnjsub.local';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    function serve(pathToFile) {
+        fs.readFile(pathToFile, (err, data) => {
+            if (err) {
+                console.logg(err);
+            } else {
+                res.end(data);
+            }
+        });
+    }
+
+    switch (req.url) {
+        case '/': {
+            serve('index.html');
+            break;
+        }
+        case '/about': {
+            serve('about.html');
+            break;
+        }
+        case '/script.js': {
+            serve('script.js');
+            break;
+        }
+    }
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
