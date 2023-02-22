840f845a8ba63f8fcb77f94505daff8235de043b
Hugo Garcia <hgarcia0805@gmail.com>
2023-02-22 15:46:11 -0500

    new exersize

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
-/*We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 
-to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+console.log(countBs("BBC"));
+// - > 2
+//console.log(countChar("kakkerlak", "k));
+// - > 4
 
-    Zero is even.
-
-    One is odd.
-
-    For any other number N, its evenness is the same as N - 2.
-
-Define a recursive function isEven corresponding to this description. 
-The function should accept a single parameter (a positive, whole number) and return a Boolean.
-
-Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
-
-Your code here. */
-
-function isEven(num) {
-    if (num == 0) {
-        return true;
-    } else if (num == 1) {
-        return false;
-    } else {
-        num - 2;
-        function isEven(num)
+function countBs(string) {
+    if ("string"[N] > 0){
+     :wq
     }
+    
 }
 
-
-
-
-console.log(isEven(50));
-// → true
-console.log(isEven(75));
-// → false
-console.log(isEven(-1));
-// → ??
