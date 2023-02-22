a04b7a7f07a9dc288c8a679f5975eba31604f60c
themoonset <hgarcia0805@gmail.com>
2023-02-22 10:26:53 -0600

    chapter 2 exersizes

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-console.log(animalPadded(3) + " Cows");
-console.log(animalPadded(6) + " chickens");
-console.log(animalPadded(14) + " peegs");
+/*We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 
+to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 
-function animalPadded(num) {
-    let animalString = String(num);
-    while (animalString.length < 3) {
-        animalString = "0" + animalString;
+    Zero is even.
+
+    One is odd.
+
+    For any other number N, its evenness is the same as N - 2.
+
+Define a recursive function isEven corresponding to this description. 
+The function should accept a single parameter (a positive, whole number) and return a Boolean.
+
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+
+Your code here. */
+
+function isEven(num) {
+    if (num == 0) {
+        return true;
+    } else if (num == 1) {
+        return false;
+    } else {
+        function isEven(num) {
+            num - 2;
+        };
     }
-    return animalString;
 }
+
+
+
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
