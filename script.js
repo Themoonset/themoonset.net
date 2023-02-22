9f001eb39ca558e1308e1723898047c3d5f56804
themoonset <hgarcia0805@gmail.com>
2023-02-22 07:21:39 -0600

    testing hook

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hey there");
+console.log(animalPadded(3) + " Cows");
+console.log(animalPadded(6) + " chickens");
+console.log(animalPadded(14) + " peegs");
+
+function animalPadded(num) {
+    let animalString = String(num);
+    while (animalString.length < 3) {
+        animalString = "0" + animalString;
+    }
+    return animalString;
+}
