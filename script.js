df95b51f237b3011e345c6da7604a8af94e9f0ab
Hugo Garcia <hgarcia0805@gmail.com>
2023-02-23 07:35:40 -0600

    chap 3 exersize

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-console.log(countBs("BBC"));
+console.log(countChar("Bofadeez", "B"));
 // - > 2
-//console.log(countChar("kakkerlak", "k));
+console.log(countChar("kakkerlak", "k"));
 // - > 4
 
-function countBs(string) {
-    if ("string"[N] > 0){
-     :wq
-    }
-    
+function countChar(string, letter) {
+    let letterCount = 0;
+    for (let count = 0; count < string.length; count++) {
+        if (string[count] == letter) {
+            letterCount++;
+        } 
+    } return letterCount;
 }
-
